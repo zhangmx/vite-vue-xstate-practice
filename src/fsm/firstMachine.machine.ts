cbce342dc79df26d8014b94c2ea519ea1703bfad
Flyman <1308646+zhangmx@users.noreply.github.com>
2023-05-08 10:04:51 +0800

    set typescript runner ts-node-esm

--- src/fsm/firstMachine.machine.ts
+++ src/fsm/firstMachine.machine.ts
@@ -3,6 +3,7 @@ import { createMachine, interpret } from 'xstate';
 const promiseMachine = createMachine({
     /** @xstate-layout N4IgpgJg5mDOIC5QAcBOB7AtgS1mAdMmAHYTbFQDEASgKIDKA8gDIBqtA2gAwC6iK6WNgAu2dMX4gAHogCMANgAsAGhABPRAA4ATPm0BWAL6HVaLLgJFS5KnQBStAMIAVbnyQhkgkWIkeZCAoq6nKKAMx6RsaqxOgQcJJmOHiJ3qLikgEAtPKqGgg50Z4YyZYkZBSpQul+oAGK2nlaAJz4CvIdnV3yAOxFSRb4qHDoADYAbpBVPhn+iNrNivj6PZqyBk0Ia8v9JYPDAFZgAMbCUx5e1b6Z84vLq+v6m7INO8aGQA */
     id: 'promise',
+    predictableActionArguments: true,
     initial: 'pending',
     states: {
         pending: {
